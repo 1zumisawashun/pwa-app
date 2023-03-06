a1fa9ebffa20e2c18b7c2b1a5ae8a308b589c653
1zumisawashun <shun.1zumisawa@gmail.com>
2023-03-06 19:35:01 +0900

    fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("./service-worker-template.js")
+      .register("./service-worker.js")
       .then((registration) => {
         console.log("ServiceWorker registration successful.");
       })
