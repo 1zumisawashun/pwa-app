9a2a56b90b9007f50b99bc796baa7269bdc4a402
1zumisawashun <shun.1zumisawa@gmail.com>
2023-03-06 15:58:52 +0900

    fix

--- service-worker.js
+++ service-worker.js
@@ -15,74 +15,38 @@ Copyright 2015, 2019, 2020, 2021 Google LLC. All Rights Reserved.
 // previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = "offline";
-
-// const appShellFiles = ["./offline.html", "./index.html", "./index2.html"];
-// const otherFiles = [];
-// const contentToCache = appShellFiles.concat(otherFiles);
-
-// self.addEventListener("install", (event) => {
-//   self.skipWaiting();
-//   evt.waitUntil(
-//     caches.open(cacheName).then(function (cache) {
-//       console.log("[Service Worker] Caching all: app shell and content");
-//       return cache.addAll(contentToCache);
-//     })
-//   );
-// });
-
-const OFFLINE_URL = "./offline.html";
+// Customize this with a different URL if needed.
+const OFFLINE_URL = "offline.html";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
-      // Setting {cache: 'reload'} in the new request will ensure that the
-      // response isn't fulfilled from the HTTP cache; i.e., it will be from
-      // the network.
+      // Setting {cache: 'reload'} in the new request will ensure that the response
+      // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
       await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
     })()
   );
-  // 待機中のサービスワーカーが強制的にアクティブなサービスワーカーにされる。
+  // Force the waiting service worker to become the active service worker.
   self.skipWaiting();
 });
 
-// self.addEventListener("activate", (event) => {
-//   event.waitUntil(
-//     (async () => {
-//       // Enable navigation preload if it's supported.
-//       // See https://developers.google.com/web/updates/2017/02/navigation-preload
-//       if ("navigationPreload" in self.registration) {
-//         await self.registration.navigationPreload.enable();
-//       }
-//     })()
-//   );
-
-//   // Tell the active service worker to take control of the page immediately.
-//   self.clients.claim();
-// });
-
-self.addEventListener("activate", function (event) {
-  console.log(event, "===========");
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    (function () {
-      caches.keys().then(function (oldCacheKeys) {
-        oldCacheKeys
-          .filter(function (key) {
-            console.log(key, "===========");
-            return key !== CACHE_NAME;
-          })
-          .map(function (key) {
-            console.log(key, "===========");
-            return caches.delete(key);
-          });
-      });
-      clients.claim();
+    (async () => {
+      // Enable navigation preload if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
     })()
   );
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("[Service Worker] Fetched resource " + event.request.url);
   // We only want to call event.respondWith() if this is a navigation request
   // for an HTML page.
   if (event.request.mode === "navigate") {
@@ -104,6 +68,10 @@ self.addEventListener("fetch", (event) => {
           // If fetch() returns a valid HTTP response with a response code in
           // the 4xx or 5xx range, the catch() will NOT be called.
           console.log("Fetch failed; returning offline page instead.", error);
+
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
         }
       })()
     );
